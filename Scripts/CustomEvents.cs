0daed7f3d3a066ff1acbe654a8a3c56236904550
Fhiz <40817126+wovencode@users.noreply.github.com>
2020-01-08 14:15:13 +0100

    Added CustomEvents
    Added CustomEvents

---
+++ Scripts/CustomEvents.cs
@@ -0,0 +1,28 @@
+﻿// =======================================================================================
+// Custom Events
+// by Weaver (Fhiz)
+// MIT licensed
+// =======================================================================================
+
+using wovencode;
+using System;
+using UnityEngine.Events;
+
+namespace wovencode
+{
+	
+	[System.Serializable]
+	public class UnityEventString : UnityEvent<string> {}
+	
+	[System.Serializable]
+	public class UnityEventLong : UnityEvent<long> {}
+	
+	[System.Serializable]
+	public class UnityEventInt : UnityEvent<int> {}
+	
+	[System.Serializable]
+	public class UnityEventBool : UnityEvent<bool> {}
+	
+}
+
+// =======================================================================================
